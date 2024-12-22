68ab09e88a8b643189c0c6e46390139c2335012d
yukky9 <kozhurinayulia@gmail.com>
2024-12-22 20:40:24 +0300

    fixed

--- src/components/atoms/Buttons/StartButton.tsx
+++ src/components/atoms/Buttons/StartButton.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const StartButton = () => {
     return (
         <button type="button"
-                className="text-white w-96 h-12 bg-yellow-400 hover:bg-yellow-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-1.5 me-2 mb-2">Запустить</button>
+                className="text-black w-96 h-12 bg-yellow-400 hover:bg-yellow-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-1.5 me-2 mb-2">Запустить</button>
     );
 };
 
