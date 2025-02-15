ca072cf76b41bfd29fd70a845da3e39851f83337
yukky9 <kozhurinayulia@gmail.com>
2025-02-15 12:18:33 +0300

    changes

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import GeneralPage from "../components/pages/GeneralPage";
 
 const MainRouter = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<GeneralPage/>} />
 			</Routes>
 		</BrowserRouter>
 	);
