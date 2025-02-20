74e36f8c218d737c7e737631c1dd29b344569bfa
yukky9 <kozhurinayulia@gmail.com>
2025-02-20 17:52:29 +0300

    fix modal admin

--- src/components/modals/staticAndPhotoModal/StaticAndPhotoModal.tsx
+++ src/components/modals/staticAndPhotoModal/StaticAndPhotoModal.tsx
@@ -5,26 +5,26 @@ import Checkbox from "../../atoms/checkbox/Checkbox";
 
 const StaticAndPhotoModal = () => {
     return (
-        <div className="grid grid-rows-2 gap-12">
-            <div className="grid grid-cols-4 gap-10">
-                <div className="grid grid-rows-2 gap-7">
-                    <img className="rounded-sm" alt='photo1'/>
+        <div className="grid">
+            <div className="grid grid-cols-4 gap-10 -mb-10">
+                <div className="grid grid-rows-2 gap-7 justify-items-center">
+                    <img className="rounded-md h-48 w-36 bg-green-400" alt='photo1'/>
                     <Checkbox/>
                 </div>
-                <div className="grid grid-rows-2 gap-7">
-                    <img className="rounded-sm" alt='photo2'/>
+                <div className="grid grid-rows-2 gap-7 justify-items-center">
+                    <img className="rounded-md h-48 w-36 bg-green-400" alt='photo2'/>
                     <Checkbox/>
                 </div>
-                <div className="grid grid-rows-2 gap-7">
-                    <img className="rounded-sm" alt='photo3'/>
+                <div className="grid grid-rows-2 gap-7 justify-items-center">
+                    <img className="rounded-md h-48 w-36 bg-green-400" alt='photo3'/>
                     <Checkbox/>
                 </div>
-                <div className="grid grid-rows-2 gap-7">
-                    <img className="rounded-sm" alt='photo4'/>
+                <div className="grid grid-rows-2 gap-7 justify-items-center">
+                    <img className="rounded-md h-48 w-36 bg-green-400" alt='photo4'/>
                     <Checkbox/>
                 </div>
             </div>
-            <div className="grid grid-cols-2 gap-10">
+            <div className="grid grid-cols-2 gap-10 -mt-20">
                 <ChangeButton/>
                 <CancelButton/>
             </div>
